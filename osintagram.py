76844c0e5e657e772ea7c8ce11c4c4548156e00f
juan garcia <juan.dw.ft@gmail.com>
2023-12-26 11:43:54 -0600

    add Media information option

--- osintagram.py
+++ osintagram.py
@@ -25,32 +25,41 @@ pc.printout("\nTarget Id: "+target_user_id+"\n", pc.GREEN)
 
 while True:
     pc.printout("\nEnter a command:\n", pc.YELLOW)
-    user_input = input("1) Get User Id\n2) Get User Media\n3)Get User Information\n4)Search Followers\n5)Search Following\n6)Search Most Recent Media/Posts By Location\n0)Exit\n")
+    user_input = input("1) Get User Id\n2) Get User Media\n3) Get User Information\n4) Search Followers\n5) Search Following\n6) Search Most Recent Media/Posts By Location\n7) Get Media Information\n0) Exit\n")
 
     # Check the user input and execute the corresponding command
     if user_input == '1':
         print(target_user_id)
     elif user_input == '2':
-        limit = input("Enter Number of Posts you want to retrieve:")
+        limit = input("Enter Number of Posts you want to retrieve:\n")
+        pc.printout("Retrieving Posts..\n", pc.MAGENTA)
         print(cl.user_medias(target_user_id, limit))
     elif user_input == '3':
+        pc.printout("Retrieving User Information..\n", pc.MAGENTA)
         print(cl.user_info(target_user_id))
     elif user_input == '4':
-        query = input("Enter Follower Username:")
+        query = input("Enter Follower Username:\n")
+        pc.printout("Retrieving Follower..\n", pc.MAGENTA)
         print(cl.search_followers(target_user_id, query))
     elif user_input == '5':
-        query = input("Enter Following Username:")
+        query = input("Enter Following Username:\n")
+        pc.printout("Retrieving Following..\n", pc.MAGENTA)
         print(cl.search_following(target_user_id, query))
     elif user_input == '6':
-        lat = input("Enter Location Latitude:")
-        lng = input("Enter Location Longitude:")
+        lat = input("Enter Location Latitude:\n")
+        lng = input("Enter Location Longitude:\n")
         location = cl.location_search(lat, lng)[0]
         location.dict()
         location = cl.location_complete(location)
-        limit = input("Enter Number of Posts you want to retrieve:")
+        limit = input("Enter Number of Posts you want to retrieve:\n")
+        pc.printout("Retrieving Recent Medias In Location..\n", pc.MAGENTA)
         print(cl.location_medias_recent(location.pk, limit))
+    elif user_input == '7':
+        media_pk = input("Enter Media Pk:\n")
+        pc.printout("Retrieving Media Information..\n", pc.MAGENTA)
+        print(cl.media_info(media_pk).dict())
     elif user_input == '0':
         pc.printout("Exiting the program. Goodbye!", pc.CYAN)
         break  # Exit the loop
     else:
-        print("Invalid command. Please enter command 1, 2, or 3.")
+        print("Please enter a valid command.")
